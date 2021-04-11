94128c3af81e22d4f0b6036c34f1ee3114b6a46f
Michael Carr <xyeres@me.com>
2021-04-11 15:06:10 -0700

    setup db and test routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('...starting')
+  let handleAsync = async (req, res, next) => {
+    let books = await Book.findAll();
+    books = books.map(book => book.toJSON())
+    console.log('got books')
+    res.json(books);
+  }
+  handleAsync(req, res, next);
+  console.log('done with get handler')
 });
 
 module.exports = router;
